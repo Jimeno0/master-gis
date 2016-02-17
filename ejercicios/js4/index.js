42b4ed479529e8172f0a9654fad618cde4af6f8d
Jimeno0 <carlospj@icloud.com>
2016-02-17 23:58:15 +0100

    ej4&5 commit

--- ejercicios/js4/index.js
+++ ejercicios/js4/index.js
@@ -3,17 +3,29 @@
 /* 
   Define un objeto con nombre 'map' vacío
 */
-
+var map = {};
 
 /*
   Define una propiedad 'setBasemap' en el objeto map que contenga una función 
   que reciba como parámetro una cadena de texto y que establezca dicha cadena
   como propiedad del objeto
 */
+map.setBasemap = function (text){
+	map.basemap = text;
+}
 
+ map.setBasemap('streets');
 
 /*
   Define una función anónima que se auto-ejecute. Dentro debe llamar a la función definida 
   en el objeto map y antes de que acabe imprima el valor de la propiedad usando console.log
 */
 
+(function(){
+  map.setBasemap('streets');
+  console.log(map);
+}());
+
+/* Me sigue diciendo que "Uncaught TypeError: map.setBasemap is not a function"
+creo que porque se lanza antes la funcion anonima de que defina la funcnion setBasemap
+*/
--- ejercicios/js5/index.js
+++ ejercicios/js5/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// Accede al elemento h1 y modifica el color
+// Accede al elemento h1 y modifica el color #C00
+var h1 = document.getElementsByTagName('h1')[0];
+h1.setAttribute('style','color:#C00');
+
+//Accede al ID header y cambia el color de fondo a 
+
+var header = document.getElementById('header');
+header.setAttribute('style','background-color: yellow');
+
+//Accede al elemento map y modifica la anchura del mapa 'width:500px'
+
+var mapa = document.getElementsByClassName('map')[0];
+mapa.setAttribute('style','width:500px');
